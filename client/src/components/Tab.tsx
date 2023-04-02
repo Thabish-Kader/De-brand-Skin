34a2b2ccd8f2543b8ec63fb61bd7cac91a687c5d
Thabish <kadertabish2@gmail.com>
2023-04-02 12:17:08 +0400

    typeings
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- client/src/components/Tab.tsx
+++ client/src/components/Tab.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-export const Tab = () => {
+type TapProps = {
+	tab: {
+		name: string;
+		icon: string;
+	};
+	handleClick: () => void;
+	isFilterTab?: boolean;
+	isActiveTab?: boolean;
+};
+export const Tab = ({ tab, handleClick }: TapProps) => {
 	return <div>Tab</div>;
 };
