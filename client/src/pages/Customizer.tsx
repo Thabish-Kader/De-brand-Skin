08dd749de10190dfad1da22961a224ddf007e2fd
Thabish <kadertabish2@gmail.com>
2023-04-02 11:58:42 +0400

    animation
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- client/src/pages/Customizer.tsx
+++ client/src/pages/Customizer.tsx
@@ -1,7 +1,66 @@
+import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
+import { useSnapshot } from "valtio";
+import state from "../store";
+import { fadeAnimation, slideAnimation } from "../config/motion";
+import { EditorTabs, FilterTabs } from "../config/constants";
+import { Tab } from "../components/Tab";
+import { CustomButton } from "../components/CustomButton";
 
 const Customizer = () => {
-	return <div>Customizer</div>;
+	const snap = useSnapshot(state);
+	return (
+		<AnimatePresence>
+			{!snap.intro && (
+				<>
+					<motion.div
+						key="custom"
+						className="absolute top-0 left-0 z-10"
+						{...slideAnimation("left")}
+					>
+						<div className="flex items-center min-h-screen">
+							<div className="editortabs-container tabs">
+								{EditorTabs.map((tab) => (
+									<Tab
+										key={tab.name}
+										tab={tab}
+										handleClick={() => {}}
+									/>
+								))}
+							</div>
+						</div>
+					</motion.div>
+
+					<motion.div
+						className="absolute z-10 top-5 right-5 "
+						{...fadeAnimation}
+					>
+						<CustomButton
+							type="filled"
+							title="Go Back"
+							handleClick={() => (state.intro = true)}
+							customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+						/>
+					</motion.div>
+
+					<motion.div
+						className="filtertabs-container"
+						{...slideAnimation("up")}
+					>
+						{FilterTabs.map((tab) => (
+							<Tab
+								key={tab.name}
+								tab={tab}
+								isFilterTab
+								isActiveTab
+								handleClick={() => {}}
+							/>
+						))}
+					</motion.div>
+				</>
+			)}
+		</AnimatePresence>
+	);
 };
 
 export default Customizer;
