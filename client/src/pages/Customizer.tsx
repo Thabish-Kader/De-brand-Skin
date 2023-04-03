f1e2e035b51349951f44000aa4440d3f52c2d156
Thabish <kadertabish2@gmail.com>
2023-04-03 13:02:40 +0400

    customizer
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- client/src/pages/Customizer.tsx
+++ client/src/pages/Customizer.tsx
@@ -70,10 +70,11 @@ const Customizer = () => {
 				break;
 			case "stylishShirt":
 				state.isFullTexture = !activeFilterTab[tabName];
-
+				break;
 			default:
 				state.isLogoTexture = true;
 				state.isFullTexture = false;
+				break;
 		}
 
 		// after setting the state, activeFilterTab is updated
