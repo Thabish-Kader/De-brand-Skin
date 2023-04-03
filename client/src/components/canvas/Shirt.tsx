707718fa7c71214f80767f2a1ccdaedb9e467de4
Thabish <kadertabish2@gmail.com>
2023-04-03 10:08:30 +0400

    decal-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- client/src/components/canvas/Shirt.tsx
+++ client/src/components/canvas/Shirt.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { easing } from "maath";
 import { useSnapshot } from "valtio";
 import state from "../../store";
-import { useGLTF, useTexture } from "@react-three/drei";
+import { Decal, useGLTF, useTexture } from "@react-three/drei";
 import { GLTF } from "three/examples/jsm/loaders/GLTFLoader";
 
 type TGltf = GLTF & {
@@ -26,7 +26,24 @@ export const Shirt = () => {
 				material={materials.lambert1}
 				material-roughness={1}
 				dispose={null}
-			></mesh>
+			>
+				{snap.isFullTexture && (
+					<Decal
+						position={[0, 0, 0]}
+						rotation={[0, 0, 0]}
+						scale={1}
+						map={fullTexture}
+					/>
+				)}
+				{snap.isLogoTexture && (
+					<Decal
+						position={[0, 0.04, 0.15]}
+						rotation={[0, 0, 0]}
+						scale={0.15}
+						map={logoTexture}
+					/>
+				)}
+			</mesh>
 		</group>
 	);
 };
